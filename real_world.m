78a2ab0b9e67698f07d74ad2171c9457187b95b5
ZacharyHeras <76194547+ZacharyHeras@users.noreply.github.com>
2022-02-02 16:36:16 -0500

    did second step on exercise

--- real_world.m
+++ real_world.m
@@ -10,6 +10,7 @@ close all; clc; clear device;
 
 %% Connect to device
 % device = open serial communication in the proper COM port
+serialport(COM9)
 
 %% Parameters
 target      = 0.5;   % Desired height of the ball [m]
