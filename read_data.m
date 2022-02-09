e85eaac5fc007ccfabe0db4c6dc1be85d75df866
ZacharyHeras <76194547+ZacharyHeras@users.noreply.github.com>
2022-02-09 16:22:47 -0500

    Completed read_data.m

--- read_data.m
+++ read_data.m
@@ -12,21 +12,26 @@ function [distance,pwm,target,deadpan] = read_data(device)
 %  system
 %
 % Created by:  Kyle Naddeo 1/3/2022
-% Modified by: YOUR NAME and DATE
+% Modified by: Zachary Heras 2/9/2022
 
 %% Ask nicely for data
 % use the serialport() command options to write the correct letter to the
 % system (Hint: the letters are in the spec sheet)
+    device.writeline('s');
 
 %% Read data
 % use the serialport() command options to read the response
+    if device.NumBytesAvailable == 20
+            response = device.readline();
+            device.flush();
+    end
 
 %% Translate
 % translate the response to 4 doubles using str2double() and
 % extractBetween() (Hint: the response is in the spec sheet)
-% distance   = 
-% manual_pwm = 
-% target     = 
-% deadpan    = 
+    distance   = str2double(extractBetween(response, 2, 5));
+    pwm = str2double(extractBetween(response, 7, 10));
+    target     = str2double(extractBetween(response, 12, 15));
+    deadpan    = str2double(extractBetween(response, 17, 20));
 
 end
